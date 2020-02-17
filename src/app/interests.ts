f5a58ef09b67d565001d6e8288ab34ed325ca182
JackLance <jack.l.lance@gmail.com>
2020-02-17 13:42:21 -0500

    comit

--- src/app/interests.ts
+++ src/app/interests.ts
@@ -15,13 +15,48 @@ export class InterestsComponent  {
   options = [
     'Bluebirds',
     'Greenpeace',
-'Orange juice',
-'Pink Floyd',
-'Purple heart',
-'Redshank',
-'Yellowstone',]
+    'Orange juice',
+    'Pink Floyd',
+    'Purple heart',
+    'Redshank',
+    'Yellowstone',]
+  code = [0,3,1,6,5]
   get asdf(){
-    return JSON.stringify(this.groups.map(x=>x.value));
+    let guess = this.groups.map(x=>x.value.ctrl);
+    let zero = 0;
+    let one = 0;
+    for(var i=0; i<4; i++){
+      if(guess[i] == this.code[i]){
+        zero++;
+      }
+      else if(guess.includes(this.code[i])){
+        one++;
+      }
+    }
+    let zeroes = [1,2,3,4,5];
+    let ones = [6,7,8,9,10];
+    let twos = [11,12,13,14,15];
+    let three = []
+    let two = 5-zero-one;
+    while(zero>0){
+      let ind = Math.floor(Math.random() * zeroes.length);
+      three.push(zeroes[ind]);
+      zeroes.splice(ind, 1);
+      zero--;
+    }
+    while(one>0){
+      let ind = Math.floor(Math.random() * ones.length);
+      three.push(zeroes[ind]);
+      ones.splice(ind, 1);
+      one--;
+    }
+    while(two>0){
+      let ind = Math.floor(Math.random() * twos.length);
+      three.push(zeroes[ind]);
+      twos.splice(ind, 1);
+      two--;
+    }
+    return JSON.stringify(three)
   }
   groups = [
     new FormGroup({ctrl:new FormControl()}),
