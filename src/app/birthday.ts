e459f993a8652b51b45689cdcd4f501c40ec3941
JackLance <jack.l.lance@gmail.com>
2020-02-17 20:38:46 -0500

    2 files modified

--- src/app/birthday.ts
+++ src/app/birthday.ts
@@ -9,12 +9,43 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class BirthdayComponent {
-  a = 1;
-  b = 1;
-  c = 1;
+  a = 0;
+  b = 0;
+  c = 0;
+  slideA;
+  slideB;
+  slideC;
+
+  init(x, y, z){
+    this.slideA = x;
+    this.slideB = y;
+    this.slideC = z;
+  }
+  aa(e){
+    let del = e.value - (this.a + 2*this.b - 0.5 * this.c);
+    this.a += del;
+    this.recalc();
+  }
 
   bb(e){
-    console.log(e);
+    let del = e.value - (-this.a+this.b+this.c);
+    this.b += del;
+        this.recalc();
+
+  }
+
+  cc (e){
+    let del = e.value - (2*this.a + 4*this.b + this.c);
+    this.c += del;
+        this.recalc();
+
+  }
+
+  recalc(){
+    this.slideA.value = this.a + 2*this.b - 0.5 * this.c;
+    this.slideB.value = (-this.a+this.b+this.c);
+    this.slideC.value = 2*this.a + 4*this.b + this.c;
+
   }
 
   dayLabel(val){
