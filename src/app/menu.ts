3eb64d13204e5adaedd981987a09f64392ea26f6
JackLance <jack.l.lance@gmail.com>
2020-02-19 16:13:01 -0500

    1 file modified

--- src/app/menu.ts
+++ src/app/menu.ts
@@ -5,6 +5,7 @@ import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 import {MatAutocompleteTrigger} from '@angular/material/autocomplete';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatMenu} from '@angular/material/menu';
 
 @Component({
   selector: 'menu',
@@ -13,17 +14,44 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 })
 export class MenuComponent  {
   
-  names=[['a', ['as', 'ad']],['s', ['sa', 'sd']],['d', ['da', 'ds']]];
-  menus = {}
-  init(a, s){
-    this.menus[s] = a;
+  nexts = {
+    'a': ['s', 'd'],
+    's': ['a', 'd'],
+    'd': ['a', 's'],
+    'start': ['a', 's', 'd', 'asdfasdf']
   }
+  namesT = []
+  names = [['start']]
+  menus = {}
+  ready = {}
+  init(a, s){
+    this.menus[s.join('')] = a;
+  }
+
+  init2(a, s){
+    let end = s[s.length-1];
+    for(let next of this.nexts[end]){
+      let newone = s.slice();
+      if(!tnexts[next]) continue;
+      newone.push(next);
+      if(this.namesT.includes(newone.join())) return;
+      this.names.push(newone);
+      this.namesT.push(newone.join())
+      //this.menus[newone] = new MatMenu();
+    }
+    setTimeout(()=>{this.ready[s] = true}, 10);
+  }
+
 
   asdf(x){
     return x;
   }
 
-  addChildren(){
+  idk(menu){
+    return (menu.length%4==1||menu.length%4==2)?'above':'below';
+  }
 
+  idk2(menu){
+    return (menu.length%4>1)?'after':'before';
   }
 }
